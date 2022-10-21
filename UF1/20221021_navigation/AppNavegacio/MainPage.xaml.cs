f8e7b8ccc43065a2bab8c7293ab0108f1d59aa95
bernat <bernat.orellana@gmail.com>
2022-10-21 18:03:23 +0200

    a

---
+++ UF1/20221021_navigation/AppNavegacio/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0xc0a
+
+namespace AppNavegacio
+{
+    /// <summary>
+    /// Página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            NavegaA();
+        }
+
+        public void NavegaA()
+        {
+            frmMain.Navigate(typeof(PaginaA));
+            
+        }
+
+        public void NavegaB()
+        {
+            frmMain.Navigate(typeof(PaginaB));
+        }
+
+        private void NavView_ItemInvoked(NavigationView sender, 
+            NavigationViewItemInvokedEventArgs args)
+        {
+            String opcio = args.InvokedItem.ToString();
+            if (opcio.Equals("Pagina A")) NavegaA();
+            else if (opcio.Equals("Pagina B")) NavegaB();
+        }
+    }
+}
