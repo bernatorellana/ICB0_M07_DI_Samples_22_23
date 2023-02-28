45249c677f77d8cab5e68dec9fbe5bbf3d26b2c6
bernat <bernat.orellana@gmail.com>
2023-02-28 15:27:07 +0100

    UF2

---
+++ UF2/20230224_TestUnitari/DemoTestingUnitari/DemoTestingUnitari/Model/Tarifes.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoTestingUnitari.Model
+{
+    public class Tarifes
+    {
+        /// <summary>
+        /// Calcula el preu de l'entrada.
+        ///  - jubilats: gratuït
+        ///  - menors: 10€
+        ///  - public general: 20€
+        /// </summary>
+        /// <param name="edat">L'edat. Ha d'estar entre 3 i 120 (inclosos) anys.
+        /// Altrament llança excepció.</param>
+        /// <returns>el preu de l'entrada</returns>
+        public static decimal getPreu(int edat)
+        {
+
+            if(edat>65)
+            {
+                return 0.00m;
+            } else if(edat < 18)
+            {
+                return 10.00m;
+            } else
+            {
+                return 20.00m;
+            }
+        }
+    }
+}
---
+++ UF2/20230224_TestUnitari/DemoTestingUnitari/UnitTestProject/TarifaTest.cs
@@ -0,0 +1,51 @@
+﻿
+using System;
+using DemoTestingUnitari.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class TarifaTest
+    {
+        [TestMethod]
+        public void kkkk()
+        {
+
+            /// <summary>
+            /// Calcula el preu de l'entrada.
+            ///  - jubilats: gratuït
+            ///  - menors: 10€
+            ///  - public general: 20€
+            /// </summary>
+            /// <param name="edat">L'edat. Ha d'estar entre 3 i 120 (inclosos) anys.
+            /// Altrament llança excepció.</param>
+            /// <returns>el preu de l'entrada</returns>
+
+            /*
+            // -1
+            //Assert.AreEqual(  )
+            //  2
+            //  3
+            Assert.AreEqual(10, Tarifes.getPreu(3));
+            // 15
+            Assert.AreEqual(10, Tarifes.getPreu(15));
+            // 17
+            Assert.AreEqual(10, Tarifes.getPreu(17));
+            // 18
+            Assert.AreEqual(20, Tarifes.getPreu(18));
+            // 45
+            Assert.AreEqual(20, Tarifes.getPreu(45));
+            // 64
+            Assert.AreEqual(20, Tarifes.getPreu(64));
+            // 65
+            Assert.AreEqual(0, Tarifes.getPreu(65));
+            // 80
+            Assert.AreEqual(0, Tarifes.getPreu(80));
+            // 120
+            // 121
+
+            Assert.AreEqual(0, 0);*/
+        }
+    }
+}
